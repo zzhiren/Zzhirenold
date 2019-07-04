81a2e4136f2d5cf4dff6bd6b971af6824f7f42f7
凯行 <506151409@qq.com>
2019-07-04 18:08:44 +0800

    _修改配置文件
    _修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,7 +16,7 @@ git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
 git push -f https://github.com/Zzhiren/zzhiren.github.io.git master
-# git push -f git@github.com:zzhiren/zzhiren.github.io.git master
+# git push -f git@github.com:Zzhiren/zzhiren.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
