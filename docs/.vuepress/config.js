625e67023f732f01d3af983b0010a62a36ebacb2
钱凯行 <zzhirensos@163.com>
2019-11-03 01:37:19 +0800

    《JavaScript高级程序设计》第4章 变量、作用域和内存问题 完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Web进化论', 
+  title: 'Zzhiren', 
   description: '纸人',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
@@ -9,20 +9,25 @@ module.exports = {
     lineNumbers: false // 代码块显示行号
   },
   themeConfig: {
+    searchMaxSuggestions: 100,
+    displayAllHeaders: true, // 默认值：false
+    // lastUpdated: 'Last Updated',
+    smoothScroll: true, // 页面滚动
     nav:[ // 导航栏配置
       {
-        text: '书籍',
+        text: '读书笔记',
         items: [
-          {text: 'JavaScript高级程序设计', link: '/JavaScript高级程序设计/' },
+          {text: '《JavaScript高级程序设计》', link: '/JavaScript高级程序设计/' },
+          {text: '《深入浅出Nodejs》', link: '/深入浅出Nodejs/' },
         ]
       },
-      // {
-      //   text:'框架',
-      //   items: [
-      //     { text:'Vue.js', link: '/Vue/' },
-      //     { text:'Egg.js', link: '/Egg/' }
-      //   ]
-      // },
+      {
+        text:'框架',
+        items: [
+          { text:'Vue.js', link: '/Vue/' },
+          { text:'Egg.js', link: '/Egg/' }
+        ]
+      },
       // {
       //   text:'HTML&&CSS',
       //   items: [
@@ -32,27 +37,77 @@ module.exports = {
       //   ]
       // }
     ],
-    sidebar:[
-      // {
-      //   title: '',
-      //   collapsable: false,
-      //   children: [
-      //     '/'
-      //   ]
-      // },
-      {
-        title: '《JavaScript高级程序设计》',
-        collapsable: false,
-        children: [ 
-          '/JavaScript高级程序设计/Chapter01.md',
-          '/JavaScript高级程序设计/Chapter02.md',
-          '/JavaScript高级程序设计/Chapter03.md',
-          '/JavaScript高级程序设计/Chapter04.md',
-          '/JavaScript高级程序设计/Exercises.md',
-        ]
-      }
-    ],
+    sidebar: {
+      '/JavaScript高级程序设计/':[
+        '',
+        '第1章JavaScript简介',
+        '第2章在HTML中使用JavaScript',
+        '第3章基本概念',
+        '第4章变量、作用域和内存问题',
+        '第5章引用类型',
+        '练习题'
+        // {
+        //   title: '第1章 JavaScript简介',
+        //   path: '第1章JavaScript简介',
+        //   collapsable: true, // 可选的, 默认值是 true,
+        // },
+        // {
+        //   title: '第2章 在HTML中使用JavaScript',
+        //   path:'第2章在HTML中使用JavaScript',
+        //   collapsable: true, // 可选的, 默认值是 true,
+        // },
+        // {
+        //   title: '第3章 基本概念',
+        //   path:'第3章基本概念',
+        //   collapsable: true, // 可选的, 默认值是 true,
+        // },
+        // {
+        //   title: '第4章 变量、作用域和内存问题',
+        //   path: '第4章变量、作用域和内存问题',
+        //   collapsable: true
+        // }
+      ],
+      '/深入浅出Nodejs/':[
+        ''
+      ],
+      '/Egg/':[
+        {
+          title: 'Egg.js',
+          children: [
+            'Egg'
+          ]
+        }
+      ],
+    },
+    // 功能完整的
+    // sidebar:{
+    //   '/JavaScript高级程序设计/':[
+    //     '第1章JavaScript简介',
+    //     '第2章在HTML中使用JavaScript.md',
+    //     '/JavaScript高级程序设计/第3章基本概念.md',
+    //     '/JavaScript高级程序设计/第4章 变量、作用域和内存问题.md',
+    //     '/JavaScript高级程序设计/练习题.md',
+    //   ],
+    //   '/Egg/':[
+    //     'Egg'
+    //   ],
+    //   '/': [
+    //     ''
+    //   ]
+    // },
     sidebarDepth: 8, // 侧边栏显示2级
     displayAllHeaders: true // 默认值：false
+  },
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/nprogress'
+  ],
+  configureWebpack:{
+    resolve: {
+      alias: {
+        '@img': 'public/img'
+      }
+    }
   }
 }
---
+++ docs/.vuepress/public/sidebar.conf.js
@@ -0,0 +1,15 @@
+modules.expport = {
+  '/JavaScript高级程序设计/':[
+    {
+      title: '《JavaScript高级程序设计》',
+      collapsable: true,
+      children: [ 
+        '/JavaScript高级程序设计/Chapter01.md',
+        '/JavaScript高级程序设计/Chapter02.md',
+        '/JavaScript高级程序设计/Chapter03.md',
+        '/JavaScript高级程序设计/Chapter04.md',
+        '/JavaScript高级程序设计/Exercises.md',
+      ]
+    }
+  ]
+}
